3b5c326e4bbeed49152e5c75f4225690e01e11e0
theabstractnft <98405223+theabstractnft@users.noreply.github.com>
2022-02-05 22:01:56 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -17,22 +17,7 @@ anime.timeline({loop: true})
     delay: 1000
   });
   
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
+//////slider 
 const slider = document.querySelector(".slider");
 const btnLeft = document.getElementById("moveLeft");
 const btnRight = document.getElementById("moveRight");
