ea4dc31d04ccb779c765a1063349ad3fbf56650b
theabstractnft <98405223+theabstractnft@users.noreply.github.com>
2022-02-06 23:55:54 +0530

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -192,4 +192,19 @@ $('body').on('click',function(){
 });
 
   
+//FAQ
+const All_Details = document.querySelectorAll('details');
+
+All_Details.forEach(deet=>{
+  deet.addEventListener('toggle', toggleOpenOneOnly)
+})
+
+function toggleOpenOneOnly(e) {
+  if (this.open) {
+    All_Details.forEach(deet=>{
+      if (deet!=this && deet.open) deet.open = false
+    });
+  }
+}
+
 
